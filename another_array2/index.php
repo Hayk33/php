773bca0434da526a5bc0fff5547f09b0d7a52a82
Hayk33 <haykharut3@gmail.com>
2017-02-28 19:15:19 +0400

    Add files via upload

---
+++ another_array2/index.php
@@ -0,0 +1,8 @@
+<?php
+$g= array('name' =>Hayk,
+      'surname' =>Harutyunyan,
+      'age' =>17,
+      'byear' =>2000,
+      'education' => array('YSU','Mushegh Ishkhan')
+ );
+echo $g['education'][0];
