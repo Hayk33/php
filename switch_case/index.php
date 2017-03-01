18bf353e406ae233d63a26ac965238e36fd6dfc3
Hayk33 <haykharut3@gmail.com>
2017-03-01 23:05:07 +0400

    Add files via upload

---
+++ switch_case/index.php
@@ -0,0 +1,11 @@
+<?php
+$g= 25;
+switch ($g) {
+	case 'text'://ete casei miji grac@ havasar e $g apa kkatarvi case i mej grvac cod@ ete voch ksharunaki urish casener stugel
+		echo "it's a text";
+		break;//break anhrajesht e vor cod@ ete chisht haytnaberi chsharunaki ayl case ner stugel
+	
+	default://ete voch mi case chisht che apa kkatarvi default i miji grvac cod@
+		echo "failed";
+		break;
+}
