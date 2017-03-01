528e667eaf879bcee075e717a35738b89e348fdb
Hayk33 <haykharut3@gmail.com>
2017-03-01 20:27:29 +0400

    Add files via upload

---
+++ Logical operations/index.php
@@ -0,0 +1,17 @@
+<?php
+$g= 25;
+$f= 17;
+$e= 18;
+$r= $e<=$g;
+$q= $e>$f;
+echo $r."<br>".$q."<hr>";
+//ktpi irar tak 1
+//              1
+//qani vor true hamarjeq e 1 in
+//isk false hamarjeq e datarkutyan
+$t = $f!=$e || $g>$f;
+$h = $g!=$f && $e!=$f;
+echo $t."<br>".$h;
+
+
+//$t $h el klinen true ev noric ktpvi 1
