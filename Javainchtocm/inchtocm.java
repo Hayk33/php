8347fb2dacd2a2c3df9886a20aadb1bf6720a054
Hayk33 <haykharut3@gmail.com>
2017-03-11 13:51:32 +0400

    Add files via upload

---
+++ Javainchtocm/inchtocm.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Ognakan {
+    public static void main(String[] args) {
+        Scanner o = new Scanner(System.in);
+        double c;
+        System.out.println("?????");
+        double a= o.nextDouble();
+        System.out.println(a*2.54+" ?????????");
+        System.out.println();
+    }
+}
